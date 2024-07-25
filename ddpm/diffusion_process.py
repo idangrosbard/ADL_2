b699fea39248ebcc0b9c366edfc293cc094f5b71
idg7 <idg7722@gmail.com>
2024-07-25 19:17:41 +0300

    Implemented training loop Implemented diffusion process

---
+++ ddpm/diffusion_process.py
@@ -0,0 +1,27 @@
+import torch
+from torch import nn, Tensor, LongTensor
+from typing import Tuple
+
+
+class DiffusionProcess(nn.Module):
+    def __init__(self, betas_t: Tensor, dim: int):
+        self.register_buffer('input_dim', dim)
+        self.register_buffer('betas_t', betas_t)
+
+        # From the definitions above equation 4
+        self.register_buffer('alphas_t', 1 - betas_t)
+        self.register_buffer('alpha_bar_t', torch.cumprod(self.alphas_t, dim=0))
+
+        # From equations 9-10
+        self.noise_distribution = torch.distributions.MultivariateNormal(torch.zeros(dim ** 2), torch.eye(dim ** 2))
+        
+
+    def sample(self, x_0: Tensor, t: LongTensor) -> Tuple[Tensor, Tensor]:
+        # x_0 of shape (batch_size, dim, dim)
+        # t of shape (batch_size,)
+        # implement the diffusion process according to equations 11-12
+        epsilon = self.noise_distribution.sample((x_0.shape[0],)).reshape(x_0.shape[0], self.input_dim, self.input_dim)
+        
+        x_t = x_0 * self.alpha_bar_t[t].sqrt() + (epsilon * (1 - self.alpha_bar_t[t]).sqrt())
+        return x_t, epsilon
+
---
+++ ddpm/trainer.py
@@ -0,0 +1,63 @@
+import torch
+from torch.optim import Optimizer
+from torch.optim.lr_scheduler import LRScheduler
+from torch import nn, Tensor, LongTensor
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+
+class Trainer(object):
+    def __init__(self, model: nn.Module, optimizer: Optimizer, scheduler: LRScheduler, diffusion_process: nn.Module, device: torch.device, summary_writer: SummaryWriter):
+        self.model = model
+        self.model.to(device)
+        self.optimizer = optimizer
+        self.scheduler = scheduler
+        self.diffusion_process = diffusion_process
+        self.diffusion_process.to(device)
+        self.device = device
+        self.summary_writer = summary_writer
+        self.total_steps = 0
+
+
+    def batch(self, x_0: Tensor, t: LongTensor, train: bool = True) -> float:
+        self.optimizer.zero_grad()
+
+        x_0 = x_0.to(self.device)
+        t = t.to(self.device)
+        x_t, epsilon = self.diffusion_process.sample(x_0, t)
+        epsilon_hat = self.model(x_t)
+        loss = ((epsilon - epsilon_hat) ** 2).mean()
+
+        if train:
+            loss.backward()
+            self.optimizer.step()
+            self.scheduler.step()
+
+        return loss.item()
+    
+
+    def epoch(self, data_loader: DataLoader, train: bool = True) -> float:
+        loss = 0
+        pbar = tqdm(data_loader, desc=f'{"train" if train else "eval"} epoch....')
+        for x_0, t in pbar:
+            b_loss = self.batch(x_0, t, train)
+            pbar.set_description(f'{"train" if train else "eval"} loss: {b_loss:.4f}')
+            self.summary_writer.add_scalar(f'{"train" if train else "eval"}/batch_loss', b_loss, self.total_steps)
+            self.total_steps += 1
+            loss += b_loss / len(data_loader)
+        return loss
+    
+
+    def train(self, train_dl: DataLoader, val_dl: DataLoader, n_epochs: int, eval_freq: int) -> float:
+        for e in range(n_epochs):
+            self.model.train()
+            train_loss = self.epoch(train_dl)
+            self.summary_writer.add_scalar(f'train/epoch_loss', train_loss, e)
+            print(f"Train loss: {train_loss:.4f}")
+            if e % eval_freq == 0:
+                with torch.no_grad():
+                    self.model.eval()
+                    val_loss = self.epoch(val_dl, train=False)
+                    self.summary_writer.add_scalar(f'eval/epoch_loss', val_loss, e)
+                    print(f"Val loss: {val_loss:.4f}")
