5a1d8c4c9e3f24f67421a77068f74298c8bc3cd5
idangrosbard <idangrosbard@mail.tau.ac.il>
2024-08-03 17:29:41 +0300

    Changed PE

--- ddpm/positional_encoding.py
+++ ddpm/positional_encoding.py
@@ -3,18 +3,24 @@ from torch import nn, Tensor
 
 
 class PositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, max_T: int = 200):
+    def __init__(self, d_model: int, max_T: int = 200, length: int = 10000, dim: int = 32):
         super().__init__()
         self.d_model = d_model
         self.max_T = max_T
+        self.length = length
+        self.dim = dim ** 2
     
     def forward(self, x: Tensor, t: Tensor) -> Tensor:
         pe = torch.zeros(x.shape[0], self.d_model ** 2, device=x.device)
         
-        # Set the positional encoding to be sinus for even indices 
-        pe[:, 0::2] = torch.sin(t.unsqueeze(-1) / (2 * self.max_T) ** (torch.arange(0, self.d_model ** 2, 2, device=t.device).float() / self.d_model ** 2)).squeeze(1)
+        denom = self.length ** (torch.arange(0, self.dim, device=x.device) / self.dim)
+        # Set the positional encoding to be sinus for even indices
+        mat = t.unsqueeze(-1) / denom
+        mat = mat.squeeze(1)
+
+        pe[:, 0::2] = torch.sin(mat[:, 0::2])
         # cosine for odd indices
-        pe[:, 1::2] = torch.cos(t.unsqueeze(-1) / (2 * self.max_T) ** (torch.arange(1, self.d_model ** 2, 2, device=t.device).float() / self.d_model ** 2)).squeeze(1)
+        pe[:, 1::2] = torch.cos(mat[:, 1::2])
 
         pe = pe.reshape(-1, 1, self.d_model, self.d_model)
         return x + pe
