db9be5d07c6b86341a66fd5519bea614edad4a4d
idangrosbard <idangrosbard@mail.tau.ac.il>
2024-08-05 19:14:32 +0300

    Fixed resblock

--- denoiser_backbones/resblock.py
+++ denoiser_backbones/resblock.py
@@ -5,6 +5,7 @@ class ResBlock(nn.Module):
     def __init__(self, n_channels: int, dropout: float = 0.5, n_convs: int = 3) -> None:
         super(ResBlock, self).__init__()
         
+        layers = []
         for _ in range(n_convs):
             layers = layers + [
                     nn.Conv2d(n_channels, n_channels, 3, 1, 1),
